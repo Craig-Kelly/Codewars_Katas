2542f202583248e1314039138e1d4a0e886a9813
Craig Kelly <craig.kelly@shopify.com>
2018-02-12 14:31:06 -0500

    Level 6 Katas

---
+++ Level6/arrayDiff.rb
@@ -0,0 +1,30 @@
+=begin
+Instructions:
+
+Your goal in this kata is to implement an difference function, which subtracts one list from another.
+It should remove all values from list a, which are present in list b.
+
+=end
+# =============================================================================
+# Do your work here
+# ==============================================================================
+def array_diff(a, b)
+
+number = b[0]
+
+if a.include?(number)
+    a.delete(number)
+  else
+end
+b.delete_at(0)
+a
+end
+# ==============================================================================
+# Examples / Test cases
+# ==============================================================================
+
+p array_diff([1,2], [1]) == [2]
+p array_diff([1,2,2], [1]) == [2,2]
+p array_diff([1,2,2], [2]) == [1]
+p array_diff([1,2,2], []) == [1,2,2]
+p array_diff([], [1,2]) == []
---
+++ Level6/digitalRoot.rb
@@ -0,0 +1,27 @@
+
+
+=begin
+Instructions:
+
+In this kata, you must create a digital root function.
+A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n.
+If that value has two digits, continue reducing in this way until a single-digit number is produced.
+This is only applicable to the natural numbers.
+
+=end
+# =============================================================================
+# Do your work here
+# ==============================================================================
+def digital_root(n)
+
+  if n.to_s.split(//).count >= 2
+    n = n.to_s.split(//).map{|n| n.to_i}.reduce(:+)
+    digital_root(n)
+  else
+    n
+  end
+end
+# ==============================================================================
+# Examples / Test cases
+# ==============================================================================
+p digital_root(16) == 7
