58efa93825bd80838ce3f900c7bbd0a6f766cc68
Craig Kelly <craig.kelly@shopify.com>
2018-02-12 21:49:56 -0500

    Level 6 Katas

---
+++ Kyu6/spinWords.rb
@@ -0,0 +1,33 @@
+# ==============================================================================
+=begin
+Instructions:
+
+Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata).
+Strings passed in will consist of only letters and spaces.
+Spaces will be included only when more than one word is present.
+
+https://www.codewars.com/kata/stop-gninnips-my-sdrow/train/ruby
+
+=end
+# ==============================================================================
+# Do your work here
+# ==============================================================================
+
+def spinWords(string)
+  #TODO
+array = string.split(" ")
+  array.each do |x|
+    total = x.split("").count
+    if total >= 5
+       x.reverse!
+     else
+    end
+  end
+  array.join(" ")
+end
+
+# ==============================================================================
+# Examples / Test cases
+# ==============================================================================
+
+p spinWords("Hey fellow warriors") == "Hey wollef sroirraw"
